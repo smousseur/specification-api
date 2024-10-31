728f69fd2a72dd3f5d7fed9caf62d9d23dcd97ab
Okita <smoussester@gmail.com>
2024-10-31 11:22:18 +0100

    Unit testing

--- specification-api/src/test/java/com/smousseur/specification/api/parser/CriteriaExpressionParserTest.java
+++ specification-api/src/test/java/com/smousseur/specification/api/parser/CriteriaExpressionParserTest.java
@@ -1,10 +1,9 @@
 package com.smousseur.specification.api.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
-import com.smousseur.specification.api.criteria.Criteria;
-import com.smousseur.specification.api.criteria.CriteriaJsonColumnType;
-import com.smousseur.specification.api.criteria.CriteriaJsonValue;
+import com.smousseur.specification.api.criteria.*;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +20,66 @@ class CriteriaExpressionParserTest {
     assertEquals("coord.x", jsonCriteria.jsonPath());
     assertEquals(CriteriaJsonColumnType.JSONB, jsonCriteria.columnType());
     assertEquals(Integer.class, jsonCriteria.value().getClass());
+  }
+
+  @Test
+  void testParseOperators() {
+    String expression = "property(location) != ?";
+    CriteriaExpressionParser parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    List<Criteria> criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.NOT_EQUALS, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) like ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.LIKE, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) in ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.IN, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) contains ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.CONTAINS, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) in ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.IN, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) > ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.GREATER_THAN, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) >= ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(
+        CriteriaOperation.GREATER_THAN_OR_EQUALS, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) < ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.LESS_THAN, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) <= ?";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(
+        CriteriaOperation.LESS_THAN_OR_EQUALS, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) isnull";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.ISNULL, ((CriteriaValue) criterias.get(0)).operation());
+    expression = "property(location) isnotnull";
+    parser = new CriteriaExpressionParser("criteria1", expression, 10);
+    criterias = parser.parse();
+    assertEquals(1, criterias.size());
+    assertSame(CriteriaOperation.ISNOTNULL, ((CriteriaValue) criterias.get(0)).operation());
   }
 }
